a4fb0fa71d392785b3aabf9133c299d0092cebbc
juancholesb <juanpcholes@gmail.com>
2026-06-22 08:38:19 -0500

    redirijo raiz a productos

--- tienda-de-reloj-django-main/entorno_proyecto/urls.py
+++ tienda-de-reloj-django-main/entorno_proyecto/urls.py
@@ -1,10 +1,9 @@
-# URL configuration for entorno_proyecto project.
-
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Incluir todas las rutas de la app; la app define la ruta raíz ('') como la vista de lista de productos.
+    path('', RedirectView.as_view(url='/productos/')),  # esto agrega
     path('', include('apps.entorno_app.urls')),
 ]
